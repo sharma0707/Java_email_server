993fd46f0a0b70d240b1dbe951d87789bd8ae785
unknown <sonu813028@gmail.com>
2023-01-20 13:21:53 +0530

    first commit

--- src/main/java/com/java_email/App.java
+++ src/main/java/com/java_email/App.java
@@ -1,70 +1,72 @@
 package com.java_email;
 
-import java.util.Properties;
+import gmail_sender.Gmail_Sender;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
-		String message = "Hello Dear, This is first from java server";
-		String to = "rpsharma7799@gmail.com";
-		String from = "tulsi21sharma@gmail.com";
-		String subject = "trail message";
-
-		sendEmail(message, to, from, subject);
+		
+//		String message = "Hello Dear, This is first from java server";
+//		String to = "rpsharma7799@gmail.com";
+//		String from = "tulsi21sharma@gmail.com";
+//		String subject = "trail message";
+//
+//		sendEmail(message, to, from, subject);
+//	}
+//
+//	private static void sendEmail(String message, String to, String from, String subject) {
+//
+//		String host = "smtp.gmail.com";
+//
+//		Properties prop = System.getProperties();
+//		System.out.println("Properties" + prop);
+//
+//		prop.put("mail.smtp.host", host);
+//		prop.put("mail.smtp.port", "465");
+//		prop.put("mail.smtp.ssl.enable", true);
+//		prop.put("mail.smtp.auth", true);
+//
+//		Session session = Session.getInstance(prop, new Authenticator() {
+//
+//			@Override
+//			protected PasswordAuthentication getPasswordAuthentication() {
+//				// TODO Auto-generated method stub
+//				return new PasswordAuthentication("tulsi21sharma@gmail.com", "P@nd@t@123456");
+//			}
+//
+//		});
+//
+//		session.setDebug(true);
+//
+//		MimeMessage mymessage = new MimeMessage(session);
+//
+//		try {
+//			mymessage.setFrom(from);
+//			mymessage.setSubject(subject);
+//			mymessage.setText(message);
+//
+//			mymessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//
+//			Transport.send(mymessage);
+//
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+		
+		Gmail_Sender gmail_sender = new Gmail_Sender();
+		String to ="";
+		String from ="";
+		String subject ="";
+		String message ="";
+		
+	boolean	b = gmail_sender.sendEmail(to, from, subject, message);
+	if(b) {
+		System.out.println("email sent sucessfully");
 	}
-
-	private static void sendEmail(String message, String to, String from, String subject) {
-
-		String host = "smtp.gmail.com";
-
-		Properties prop = System.getProperties();
-		System.out.println("Properties" + prop);
-
-		prop.put("mail.smtp.host", host);
-		prop.put("mail.smtp.port", "465");
-		prop.put("mail.smtp.ssl.enable", true);
-		prop.put("mail.smtp.auth", true);
-
-		Session session = Session.getInstance(prop, new Authenticator() {
-
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				// TODO Auto-generated method stub
-				return new PasswordAuthentication("tulsi21sharma@gmail.com", "P@nd@t@123456");
-			}
-
-		});
-
-		session.setDebug(true);
-
-		MimeMessage mymessage = new MimeMessage(session);
-
-		try {
-			mymessage.setFrom(from);
-			mymessage.setSubject(subject);
-			mymessage.setText(message);
-
-			mymessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-			Transport.send(mymessage);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	else {
+		System.out.println("something went wrong");
 	}
+		
 }
+	}
---
+++ src/main/java/gmail_sender/Gmail_Sender.java
@@ -0,0 +1,52 @@
+package gmail_sender;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+public class Gmail_Sender {
+	
+	public boolean sendEmail(String to, String from, String subject, String message) {
+		
+		boolean flag = false;
+		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", true);
+		properties.put("mail.smtp.starttls.enable", true);
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.host","smtp.gmail.com" );
+		
+		
+		String username = "aws7530";
+		String password = "";
+		
+		jakarta.mail.Session session = jakarta.mail.Session.getInstance(properties, new jakarta.mail.Authenticator() {
+			@Override
+			protected jakarta.mail.PasswordAuthentication getPasswordAuthentication() {
+				// TODO Auto-generated method stub
+				return new jakarta.mail.PasswordAuthentication(username, password);
+			}
+		});
+		
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			msg.setFrom(new InternetAddress(from));
+			msg.setSubject(subject);
+			msg.setText(message);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		return false;
+		
+	}
+
+}
